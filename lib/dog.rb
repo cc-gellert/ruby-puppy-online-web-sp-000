3424f38ca304106ef9435143f445058fd5cff3ef
Chloe Gell <gellert.chloe@gmail.com>
2020-01-31 03:17:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Add your code here
+class Dog 
+  def initialize
+    save 
+  end 
+  @@all = []
+  def self.all 
+    @@all 
+  end 
+  def self.print_all 
+    @@all.each do |dog|
+      puts "#{dog}"
+    end 
+  end 
+  def self.save 
+    @@all << self 
+  end 
+end 
